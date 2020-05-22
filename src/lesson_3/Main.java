3a1a89ad2950efe7763df4a07f8e31e3af755152
elka74 <mikheeva@1454.ru>
2020-05-22 11:34:18 +0300

    Initial commit

---
+++ src/lesson_3/Main.java
@@ -0,0 +1,24 @@
+package lesson_3;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Main {
+    public static void main(String[] args){
+            List<String> list = new ArrayList<>();
+            list.add("Мария");
+            list.add("Анастасия");
+            list.add("Андрей");
+            list.add("Мария");
+            list.add("Игорь");
+            list.add("Андрей");
+            list.add("Анастасия");
+            list.add("Мария");
+            list.add("Александр");
+            list.add("Анастасия");
+            list.add("Елена");
+            System.out.println(list);
+        }
+    }
+
+
+
